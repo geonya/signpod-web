db5f6f0981fca6510d85f3428d65d2e33ab6af10
geonya <geony8410@gmail.com>
2022-09-28 20:14:00 +0900

    credential

--- next.config.js
+++ next.config.js
@@ -4,14 +4,6 @@ const securityHeaders = [
     key: 'Referrer-Policy',
     value: 'origin-when-cross-origin',
   },
-  {
-    key: 'Access-Control-Allow-Credentials',
-    value: true,
-  },
-  {
-    key: 'Access-Control-Allow-Headers',
-    value: 'Set-Cookie, Cookie',
-  },
 ]
 const nextConfig = {
   reactStrictMode: true,
