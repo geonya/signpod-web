a2c5f1f596611d4e3d1aa119b4116d1560f1df37
geonya <geony8410@gmail.com>
2022-10-23 10:45:16 +0900

    221023 WORKING : work list filter

--- src/components/works/work-list-filters.tsx
+++ src/components/works/work-list-filters.tsx
@@ -1,4 +1,6 @@
-import { FC, useState } from 'react'
+import { Search } from '@mui/icons-material'
+import { Box } from '@mui/material'
+import { ChangeEvent, FC, KeyboardEvent, useMemo, useState } from 'react'
 import { useUpdateEffect } from '../../hooks/use-update-effect'
 
 interface WorkFilters {
@@ -8,7 +10,7 @@ interface WorkFilters {
 }
 
 interface FilterItem {
-  label: string
+  label: '이름' | '카테고리' | '상태'
   field: 'name' | 'category' | 'status'
   value: unknown
   displayValue?: unknown
@@ -57,7 +59,7 @@ export const WorkListFilters: FC<WorkListFiltersProps> = ({
       category: [],
       status: [],
     }
-    filterItems.map((item) => {
+    filterItems.forEach((item) => {
       switch (item.field) {
         case 'name':
           filters.name = item.value as string
@@ -73,6 +75,136 @@ export const WorkListFilters: FC<WorkListFiltersProps> = ({
     onChange?.(filters)
   }, [filterItems])
 
-  const handleDelete = (filterItem: FilterItem): void => {}
-  return <div {...other}></div>
+  const handleDelete = (filterItem: FilterItem): void => {
+    setFilterItems((prev) =>
+      prev.filter(
+        (item) =>
+          !(filterItem.field === item.field && filterItem.value === item.value),
+      ),
+    )
+  }
+
+  const handleQueryChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setQueryValue(event.target.value)
+  }
+
+  const handleQueryKeyup = (event: KeyboardEvent<HTMLInputElement>): void => {
+    if (event.code === 'Enter' && queryValue) {
+      const filterItem = filterItems.find((item) => item.field === 'name')
+      if (filterItem) {
+        setFilterItems((prev) =>
+          prev.map((item) => {
+            if (item.field === 'name') {
+              return {
+                ...item,
+                value: queryValue,
+              }
+            }
+            return item
+          }),
+        )
+      } else {
+        setFilterItems((prev) => [
+          ...prev,
+          { label: '이름', field: 'name', value: queryValue },
+        ])
+      }
+      setQueryValue('')
+    }
+  }
+
+  const handleCategoryChange = (values: string[]): void => {
+    setFilterItems((prev) => {
+      const valuesFound: string[] = []
+      const newFilterItems = prev.filter((item) => {
+        if (item.field !== 'category') {
+          return true
+        }
+        const found = values.includes(item.value as string)
+        if (found) {
+          valuesFound.push(item.value as string)
+        }
+        return found
+      })
+      if (values.length === valuesFound.length) {
+        return newFilterItems
+      }
+      values.forEach((value) => {
+        if (!valuesFound.includes(value)) {
+          const option = categoryOptions.find(
+            (option) => option.value === value,
+          )
+          newFilterItems.push({
+            label: '카테고리',
+            field: 'category',
+            value,
+            displayValue: option!.label,
+          })
+        }
+      })
+      return newFilterItems
+    })
+  }
+  const handleStatusChange = (values: string[]): void => {
+    setFilterItems((prev) => {
+      const valuesFound: string[] = []
+      const newFilterItems = prev.filter((item) => {
+        if (item.field !== 'status') {
+          return true
+        }
+        const found = values.includes(item.value as string)
+        if (found) {
+          valuesFound.push(item.value as string)
+        }
+        return found
+      })
+      if (values.length === valuesFound.length) {
+        return newFilterItems
+      }
+      values.forEach((value) => {
+        if (!valuesFound.includes(value)) {
+          const option = statusOptions.find((option) => option.value === value)
+          newFilterItems.push({
+            label: '상태',
+            field: 'status',
+            value,
+            displayValue: option!.label,
+          })
+        }
+      })
+      return newFilterItems
+    })
+  }
+
+  // prevent re-render issues
+  const categoryValues = useMemo(
+    () =>
+      filterItems
+        .filter((filterItems) => filterItems.field === 'category')
+        .map((filterItems) => filterItems.value) as string[],
+    [filterItems],
+  )
+
+  const statusValues = useMemo(
+    () =>
+      filterItems
+        .filter((filterItems) => filterItems.field === 'status')
+        .map((filterItems) => filterItems.value) as string[],
+    [filterItems],
+  )
+
+  return (
+    <div {...other}>
+      <Box
+        sx={{
+          alignItems: 'center',
+          display: 'flex',
+          p: 2,
+        }}
+      >
+        <Search fontSize='small' />
+        <Box sx={{ flexGrow: 1, ml: 3 }} />
+      </Box>
+    </div>
+  )
 }
